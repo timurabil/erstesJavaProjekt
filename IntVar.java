edcb7c9bb56b3a6eac18a98c64e6e22d5df46d1d
timurabil <79519399+timurabil@users.noreply.github.com>
2021-02-24 20:44:58 +0100

    Add files via upload

---
+++ IntVar.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+
+public final class IntVar {
+	private IntVar()  { }
+	
+	private static final Scanner EINGABE = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		final int max = 0x7FFFFFFF;
+		final int min = 0x80000000;
+		
+		System.out.printf("Zwei ganze Zahlen zwischen %d und %d eingeben: %n" ,min, max) ;
+		
+		
+		int z1 = 0;
+		
+		int z2 = 0;
+		int k = 0;
+		
+		if (EINGABE.hasNextInt() )  {
+			z1 = EINGABE.nextInt();
+			k++;
+		} else {
+			System.out.println("ungueltige Eingabe!") ;
+		}
+		
+		if (k == 1) {
+			if (EINGABE.hasNextInt())  {
+				z2 = EINGABE.nextInt();
+				k++;
+			} else {
+				System.out.println("Ungueltige Eingabe!");
+				
+			}
+		}
+		
+		if (k == 2) {
+			System.out.printf("%d ist oktal %o und hexadezimal %x%n",
+					z1, z1, z1);
+			System.out.printf("%d ist oktal %o und hexadezimal %x%n",
+					z2, z2, z2);
+			System.out.println("waehlen Sie den Arithmetischen/Vergleichsoperator: ");
+			String operator = EINGABE.next();
+			switch (operator) {
+			case "+":
+				System.out.printf("%d + %d ist %d%n" , z1, z2, z1 + z2);
+				break;
+			case "-":
+				System.out.printf("%d - %d ist %d%n", z1, z2, z1 - z2);
+				break;
+			case "*":
+				System.out.printf("%d * %d ist %d%n", z1, z2, z1 * z2);
+				break;
+			case "/":
+				System.out.printf("%d / %d ist %d%n", z1, z2, z1 / z2);
+				break;
+			case "%":
+				System.out.printf("%d % %d ist %d%n", z1, z2, z1 % z2);
+				break;
+		}
+	}
+
+}
+}
+	
